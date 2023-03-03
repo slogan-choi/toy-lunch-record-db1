65755a5449c293cd55845f19acde574b21a9bcde
slogan-choi <slogan.choi@gmail.com>
2023-03-03 11:18:12 +0900

    feat: 테이블 관리

---
+++ sql/schema.sql
@@ -0,0 +1,12 @@
+drop table lunch if exists cascade;
+create table lunch (
+    id integer not null auto_increment,
+    restaurant varchar(255) not null,
+    menu varchar(255) not null,
+    image binary large object not null,
+    price numeric not null,
+    grade real not null,
+    updateAt time not null,
+    createAt time not null,
+    primary key (id)
+)
