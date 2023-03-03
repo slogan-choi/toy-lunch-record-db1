89c3b56b9d32ccc3380289aa5e5c03597a3d76ae
slogan-choi <slogan.choi@gmail.com>
2023-03-03 09:47:21 +0900

    feat: 프로젝트 초기 셋팅

---
+++ src/main/java/lunch/record/RecordApplication.java
@@ -0,0 +1,13 @@
+package lunch.record;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordApplication.class, args);
+	}
+
+}
