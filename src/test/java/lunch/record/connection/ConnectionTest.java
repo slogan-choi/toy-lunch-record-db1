e31fe2a3d434e539b9a873796a2f41a49551f36f
slogan-choi <slogan.choi@gmail.com>
2023-03-04 09:27:04 +0900

    feat: 스프링이 제공하는 DataSource 인터페이스가 적용된 DriverManager 인 DriverManagerDataSource 를 통해서 커넥션 획득 테스트 생성

--- src/test/java/lunch/record/connection/ConnectionTest.java
+++ src/test/java/lunch/record/connection/ConnectionTest.java
@@ -2,6 +2,7 @@ package lunch.record.connection;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -19,6 +20,20 @@ class ConnectionTest {
     void driverManager() throws SQLException {
         Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        // DriverManagerDataSource - 항상 새로운 커넥션 획득
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        useDataSource(dataSource);
+    }
+
+    private void useDataSource(DriverManagerDataSource dataSource) throws SQLException {
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
         log.info("connection={}, class={}", con1, con1.getClass());
         log.info("connection={}, class={}", con2, con2.getClass());
     }
