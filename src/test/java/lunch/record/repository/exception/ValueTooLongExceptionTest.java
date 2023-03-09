4522f3b404053be3985f2e78167c374f169b263d
slogan-choi <slogan.choi@gmail.com>
2023-03-09 15:16:41 +0900

    feat: 직접 만든 데이터 접근 예외 테스트 생성

---
+++ src/test/java/lunch/record/repository/exception/ValueTooLongExceptionTest.java
@@ -0,0 +1,68 @@
+package lunch.record.repository.exception;
+
+import lunch.record.domain.LunchRecord;
+import lunch.record.repository.LunchRecordRepository;
+import lunch.record.repository.LunchRecordRepositoryInterface;
+import lunch.record.service.LunchRecordService;
+import lunch.record.util.Utils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+
+import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.SQLException;
+import java.time.LocalTime;
+
+@SpringBootTest
+class ValueTooLongExceptionTest {
+
+    @Autowired
+    LunchRecordRepositoryInterface repository;
+    @Autowired
+    LunchRecordService service;
+    @Autowired
+    DataSource dataSource;
+
+    @TestConfiguration
+    static class TestConfig {
+        @Autowired
+        DataSource dataSource;
+
+        @Bean
+        public LunchRecordRepositoryInterface lunchRecordRepository() {
+            return new LunchRecordRepository(dataSource);
+        }
+
+        @Bean
+        public LunchRecordService lunchRecordService() {
+            return new LunchRecordService(lunchRecordRepository());
+        }
+    }
+
+    @AfterEach
+    void after() {
+        repository.deleteAll();
+    }
+
+    @Test
+    void valueTooLongSave() throws SQLException {
+        StringBuilder restaurant = new StringBuilder();
+        restaurant.append("test");
+
+        StringBuilder menu = new StringBuilder();
+        menu.append("The Double Ristretto Venti Half-Soy Nonfat Decaf Organic Chocolate Brownie Iced Vanilla Double-Shot Gingerbread Frappuccino Extra Hot With Foam Whipped Cream Upside Down Double Blended, One Sweet'N Low and One Nutrasweet, and Ice");
+        menu.append("The Double Ristretto Venti Half-Soy Nonfat Decaf Organic Chocolate Brownie Iced Vanilla Double-Shot Gingerbread Frappuccino Extra Hot With Foam Whipped Cream Upside Down Double Blended, One Sweet'N Low and One Nutrasweet, and Ice");
+
+        Blob blob = DataSourceUtils.getConnection(dataSource).createBlob();
+        blob.setBytes(1, Utils.imageToByteArray("/Users/ghc/development/img/test.png"));
+        LocalTime createAt = LocalTime.now();
+
+        service.create(new LunchRecord(restaurant.toString(), menu.toString(), blob, BigDecimal.ONE, 4.0f, 4.0f, createAt, createAt));
+    }
+}
