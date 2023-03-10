249ac32baa5b2af7f3170735639b3545edd5ab79
slogan-choi <slogan.choi@gmail.com>
2023-03-10 11:04:51 +0900

    feat: JdbcTemplate 추가로 JDBC 반복 문제 해결

--- src/main/java/lunch/record/repository/LunchRecordRepository.java
+++ src/main/java/lunch/record/repository/LunchRecordRepository.java
@@ -2,12 +2,14 @@ package lunch.record.repository;
 
 import lombok.extern.slf4j.Slf4j;
 import lunch.record.domain.LunchRecord;
-import org.springframework.jdbc.datasource.DataSourceUtils;
-import org.springframework.jdbc.support.JdbcUtils;
-import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
-import org.springframework.jdbc.support.SQLExceptionTranslator;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.lob.DefaultLobHandler;
 
 import javax.sql.DataSource;
+import javax.sql.rowset.serial.SerialBlob;
 import java.math.BigDecimal;
 import java.sql.Blob;
 import java.sql.Connection;
@@ -25,277 +27,89 @@ import java.util.Objects;
 @Slf4j
 public class LunchRecordRepository implements LunchRecordRepositoryInterface {
 
-    // [DI + OCP]
-    // LunchRecordRepository 는 DataSource 인터페이스에만 의존하기 때문에 DataSource 구현체를 변경해도 LunchRecordRepository 의 코드는 전혀 변경하지 않아도 된다.
-    private final DataSource dataSource;
-    private final SQLExceptionTranslator exTranslator;
+    private final JdbcTemplate template;
 
     public LunchRecordRepository(DataSource dataSource) {
-        this.dataSource = dataSource;
-        this.exTranslator = new SQLErrorCodeSQLExceptionTranslator(dataSource);
+        template = new JdbcTemplate(dataSource);
     }
 
     @Override
     public LunchRecord save(LunchRecord lunchRecord) {
         String sql = "insert into lunchRecord(restaurant, menu, image, price, grade, averageGrade, updateAt, createAt) values(?, ?, ?, ?, ?, ?, ?, ?)";
-        Connection con = null;
-        PreparedStatement pstmt = null;
+        LocalTime now = LocalTime.now();
+        GeneratedKeyHolder generatedKeyHolder = new GeneratedKeyHolder();
 
-        try {
-            con = getConnection();
-            pstmt = con.prepareStatement(sql);
+        template.update(con -> {
+            PreparedStatement pstmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             pstmt.setString(1, lunchRecord.getRestaurant());
             pstmt.setString(2, lunchRecord.getMenu());
             pstmt.setBinaryStream(3, lunchRecord.getImage().getBinaryStream());
             pstmt.setBigDecimal(4, lunchRecord.getPrice());
             pstmt.setFloat(5, lunchRecord.getGrade());
             pstmt.setFloat(6, lunchRecord.getAverageGrade());
-            pstmt.setTime(7, Time.valueOf(lunchRecord.getUpdateAt()));
-            pstmt.setTime(8, Time.valueOf(lunchRecord.getCreateAt()));
-            pstmt.executeUpdate();
-            return lunchRecord;
-        } catch (SQLException e) {
-            throw Objects.requireNonNull(exTranslator.translate("save", sql, e));
-        } finally {
-            close(con, pstmt, null);
-        }
+            pstmt.setTime(7, Time.valueOf(now));
+            pstmt.setTime(8, Time.valueOf(now));
+            return pstmt;
+        }, generatedKeyHolder);
+
+        return findById(Objects.requireNonNull(generatedKeyHolder.getKey()).intValue());
     }
 
     @Override
     public List<LunchRecord> findAll() {
         String sql = "select * from LunchRecord";
-
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            con = getConnection();
-            pstmt = con.prepareStatement(sql);
-
-            rs = pstmt.executeQuery();
-
-            List<LunchRecord> lunchRecordList = new ArrayList<>();
-
-            while (rs.next()) {
-                LunchRecord lunchRecord = new LunchRecord();
-                lunchRecord.setId(rs.getInt("id"));
-                lunchRecord.setRestaurant(rs.getString("restaurant"));
-                lunchRecord.setMenu(rs.getString("menu"));
-
-                Blob blob = getConnection().createBlob();
-                blob.setBytes(1, rs.getBlob("image").getBytes(1, (int) rs.getBlob("image").length()));
-
-                lunchRecord.setImage(blob);
-                lunchRecord.setPrice(rs.getBigDecimal("price"));
-                lunchRecord.setGrade(rs.getFloat("grade"));
-                lunchRecord.setAverageGrade(rs.getFloat("averageGrade"));
-                lunchRecord.setUpdateAt(rs.getTime("updateAt").toLocalTime());
-                lunchRecord.setCreateAt(rs.getTime("createAt").toLocalTime());
-                lunchRecordList.add(lunchRecord);
-            }
-
-//            if (lunchRecordList.isEmpty()) {
-//                throw new NoSuchElementException();
-//            }
-
-            return lunchRecordList;
-        } catch (SQLException e) {
-            throw Objects.requireNonNull(exTranslator.translate("findAll", sql, e));
-        } finally {
-            close(con, pstmt, rs);
-        }
+        return template.query(sql, lunchRecordRowMapper());
     }
 
     @Override
     public LunchRecord findById(int id) {
         String sql = "select * from lunchRecord where id = ?";
-
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            con = getConnection();
-            pstmt = con.prepareStatement(sql);
-            pstmt.setInt(1, id);
-            rs = pstmt.executeQuery();
-
-            if (rs.next()) {
-                LunchRecord lunchRecord = new LunchRecord();
-                lunchRecord.setId(rs.getInt("id"));
-                lunchRecord.setRestaurant(rs.getString("restaurant"));
-                lunchRecord.setMenu(rs.getString("menu"));
-
-                Blob blob = getConnection().createBlob();
-                blob.setBytes(1, rs.getBlob("image").getBytes(1, (int) rs.getBlob("image").length()));
-
-                lunchRecord.setImage(blob);
-                lunchRecord.setPrice(rs.getBigDecimal("price"));
-                lunchRecord.setGrade(rs.getFloat("grade"));
-                lunchRecord.setAverageGrade(rs.getFloat("averageGrade"));
-                lunchRecord.setUpdateAt(rs.getTime("updateAt").toLocalTime());
-                lunchRecord.setCreateAt(rs.getTime("createAt").toLocalTime());
-                return lunchRecord;
-            } else {
-                throw new NoSuchElementException();
-            }
-        } catch (SQLException e) {
-            throw Objects.requireNonNull(exTranslator.translate("findById", sql, e));
-        } finally {
-            close(con, pstmt, rs);
-        }
+        return template.queryForObject(sql, lunchRecordRowMapper(), id);
     }
 
     @Override
     public List<LunchRecord> findByRestaurantMenu(String restaurant, String menu) {
         String sql = "select * from LunchRecord where restaurant = ? and menu = ?";
-
-        Connection con = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            con = getConnection();
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, restaurant);
-            pstmt.setString(2, menu);
-            rs = pstmt.executeQuery();
-
-            List<LunchRecord> lunchRecordList = new ArrayList<>();
-
-            while (rs.next()) {
-                LunchRecord lunchRecord = new LunchRecord();
-                lunchRecord.setId(rs.getInt("id"));
-                lunchRecord.setRestaurant(rs.getString("restaurant"));
-                lunchRecord.setMenu(rs.getString("menu"));
-
-                Blob blob = getConnection().createBlob();
-                blob.setBytes(1, rs.getBlob("image").getBytes(1, (int) rs.getBlob("image").length()));
-
-                lunchRecord.setImage(blob);
-                lunchRecord.setPrice(rs.getBigDecimal("price"));
-                lunchRecord.setGrade(rs.getFloat("grade"));
-                lunchRecord.setAverageGrade(rs.getFloat("averageGrade"));
-                lunchRecord.setUpdateAt(rs.getTime("updateAt").toLocalTime());
-                lunchRecord.setCreateAt(rs.getTime("createAt").toLocalTime());
-
-                lunchRecordList.add(lunchRecord);
-            }
-
-//            if (lunchRecordList.isEmpty()) {
-//                throw new NoSuchElementException();
-//            }
-
-            return lunchRecordList;
-        } catch (SQLException e) {
-            throw exTranslator.translate("findByRestaurantMenu", sql, e);
-        } finally {
-            close(con, pstmt, rs);
-        }
+        return template.query(sql, lunchRecordRowMapper(), restaurant, menu);
     }
 
     @Override
     public void update(int id, String restaurant, String menu, Blob image, BigDecimal price, float grade) {
         String sql = "update lunchRecord set restaurant = ?, menu = ?, image = ?, price = ?, grade = ?, updateAt = ? where id = ?";
-
-        Connection con = null;
-        PreparedStatement pstmt = null;
-
-        try {
-            con = getConnection();
-            pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, restaurant);
-            pstmt.setString(2, menu);
-            pstmt.setBinaryStream(3, image.getBinaryStream());
-            pstmt.setBigDecimal(4, price);
-            pstmt.setFloat(5, grade);
-            pstmt.setTime(6, Time.valueOf(LocalTime.now()));
-            pstmt.setInt(7, id);
-
-            int resultSize = pstmt.executeUpdate();
-            log.info("resultSize={}", resultSize);
-        } catch (SQLException e) {
-            throw Objects.requireNonNull(exTranslator.translate("update", sql, e));
-        } finally {
-            close(con, pstmt, null);
-        }
+        template.update(sql, restaurant, menu, image, price, grade, LocalTime.now(), id);
     }
 
     @Override
     public void updateAverageGradeByRestaurantMenu(Float averageGrade, String restaurant, String menu) {
         String sql = "update lunchRecord set averageGrade = ?, updateAt = ? where restaurant = ? and menu = ?";
-
-        Connection con = null;
-        PreparedStatement pstmt = null;
-
-        try {
-            con = getConnection();
-            pstmt = con.prepareStatement(sql);
-            pstmt.setFloat(1, averageGrade);
-            pstmt.setTime(2, Time.valueOf(LocalTime.now()));
-            pstmt.setString(3, restaurant);
-            pstmt.setString(4, menu);
-
-            int resultSize = pstmt.executeUpdate();
-            log.info("resultSize={}", resultSize);
-        } catch (SQLException e) {
-            throw exTranslator.translate("updateAverageGradeByRestaurantMenu", sql, e);
-        } finally {
-            close(con, pstmt, null);
-        }
+        template.update(sql, averageGrade, LocalTime.now(), restaurant, menu);
     }
 
     @Override
     public void delete(int id) {
         String sql = "delete from lunchRecord where id = ?";
-
-        Connection con = null;
-        PreparedStatement pstmt = null;
-
-        try {
-            con = getConnection();
-            pstmt = con.prepareStatement(sql);
-            pstmt.setInt(1, id);
-
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            throw exTranslator.translate("delete", sql, e);
-        } finally {
-            close(con, pstmt, null);
-        }
+        template.update(sql, id);
     }
 
     @Override
     public void deleteAll() {
         String sql = "delete from lunchRecord";
-
-        Connection con = null;
-        PreparedStatement pstmt = null;
-
-        try {
-            con = getConnection();
-            pstmt = con.prepareStatement(sql);
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            throw exTranslator.translate("deleteAll", sql, e);
-        } finally {
-            close(con, pstmt, null);
-        }
-
+        template.update(sql);
     }
 
-    private void close(Connection con, Statement stmt, ResultSet rs) {
-        JdbcUtils.closeResultSet(rs);
-        JdbcUtils.closeStatement(stmt);
-        // 주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다.
-        DataSourceUtils.releaseConnection(con, dataSource);
-    }
-
-    private Connection getConnection() throws SQLException {
-        // 주의! 트랜잭션 동기화를 사용하려면 DataSourceUtils를 사용해야 한다.
-        Connection con = DataSourceUtils.getConnection(dataSource);
-        log.info("get connection={}, class={}", con, con.getClass());
-        return con;
+    private RowMapper<LunchRecord> lunchRecordRowMapper() {
+        return (rs, rowNum) -> {
+            LunchRecord lunchRecord = new LunchRecord();
+            lunchRecord.setId(rs.getInt("id"));
+            lunchRecord.setRestaurant(rs.getString("restaurant"));
+            lunchRecord.setMenu(rs.getString("menu"));
+            lunchRecord.setImage(new SerialBlob(Objects.requireNonNull(new DefaultLobHandler().getBlobAsBytes(rs, "image"))));
+            lunchRecord.setPrice(rs.getBigDecimal("price"));
+            lunchRecord.setGrade(rs.getFloat("grade"));
+            lunchRecord.setAverageGrade(rs.getFloat("averageGrade"));
+            lunchRecord.setUpdateAt(rs.getTime("updateAt").toLocalTime());
+            lunchRecord.setCreateAt(rs.getTime("createAt").toLocalTime());
+            return lunchRecord;
+        };
     }
 }
